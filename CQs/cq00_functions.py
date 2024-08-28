c1aad93515f83c92b10517f29b83fa67d1bca9cf
Tejas <tparamkusham@gmail.com>
2024-08-28 09:39:24 -0400

    FInished CQ00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""Practicing functions"""
+
+__author__ = "730767370"
+
+
+def mimic(message: str) -> str:
+    """Return the inut message"""
+    return message
+
+
+def main() -> None:
+    """Run other functions"""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
